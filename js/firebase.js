87b66597fd2e4bc8df324f1cb32797f69af1ce27
mohammd Bara <azmohammd10@gmail.com>
2026-06-28 09:32:49 +0300

    تعديل كود فيرباص

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,21 @@
+// =========================
+// STAR Firebase
+// =========================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// إعدادات مشروعك
- const firebaseConfig = { 
-  apiKey : "AIzaSyCEuOcQIvg-chEXfwHM8JAxFeYGpaOIoP4" , 
-  authDomain : "star-social-bdfb2.firebaseapp.com" , 
-  معرّف المشروع : "star-social-bdfb2" ، 
-  storageBucket : "star-social-bdfb2.firebasestorage.app" , 
-  messagingSenderId : "256449419301" , 
-  معرف التطبيق : "1:256449419301:web:970b4905d6beb86c4087cc " 
-  معرف القياس : "G-JYT3F33K7Y" 
+const firebaseConfig = {
+    apiKey: "AIzaSyCEuOcQIvg-chEXfwHM8JAxFeYGpaOIoP4",
+    authDomain: "star-social-bdfb2.firebaseapp.com",
+    projectId: "star-social-bdfb2",
+    storageBucket: "star-social-bdfb2.firebasestorage.app",
+    messagingSenderId: "256449419301",
+    appId: "1:256449419301:web:970b4905d6beb86c4087cc",
+    measurementId: "G-JYT3F33K7Y"
 };
- 
 
 // تشغيل Firebase
 const app = initializeApp(firebaseConfig);
