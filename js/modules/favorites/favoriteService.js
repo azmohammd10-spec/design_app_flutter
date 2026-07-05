19fe40da4e6ef44d05c692f86479786f02f71952
mohammd Bara <azmohammd10@gmail.com>
2026-07-05 14:17:00 +0300

    Add functions to manage favorite items in localStorage

--- js/modules/favorites/favoriteService.js
+++ js/modules/favorites/favoriteService.js
@@ -1,1 +1,15 @@
+export function getFavorites() {
+  return JSON.parse(localStorage.getItem("favorites")) || [];
+}
 
+export function toggleFavorite(id) {
+  let fav = getFavorites();
+
+  if (fav.includes(id)) {
+    fav = fav.filter(f => f !== id);
+  } else {
+    fav.push(id);
+  }
+
+  localStorage.setItem("favorites", JSON.stringify(fav));
+}
